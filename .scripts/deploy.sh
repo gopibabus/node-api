a16eb63aa7ff37b0b62f9dec473226177ed0e010
Gopibabu Srungavarapu <gsrungavarapu@tmhcc.com>
2023-10-23 22:13:21 -0400

    updated deployment github action

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,14 +3,14 @@
 set -e
 
 NODE_PATH=/root/.nvm/versions/node/v18.10.0/bin/node
-# PM2_PATH=/root/.nvm/versions/node/v18.10.0/bin/pm2
+PM2_PATH=/root/.nvm/versions/node/v18.10.0/bin/pm2
 NPM_PATH=/root/.nvm/versions/node/v18.10.0/bin/npm
 
 echo "Deployment started ..."
 
 # Enter maintenance mode or return true
 # if already is in maintenance mode
-($NODE_PATH $NPM_PATH run stop --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+($NODE_PATH $PM2_PATH stop server.js --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
 
 # Pull the latest version of the app
 git fetch origin master
@@ -18,9 +18,9 @@ git reset --hard origin/master
 git pull origin master
 
 # Compile npm assets
-$NODE_PATH $NPM_PATH install --omit=dev
+$NPM_PATH install --omit=dev
 
 # Exit maintenance mode
-$NODE_PATH $NPM_PATH run start
+$NODE_PATH $PM2_PATH start server.js
 
 echo "Deployment finished!"
